a2c1167bed1a2ecb7f9d095861dcece9038dbdce
SkuldNorniern <skuldnorniern@gmail.com>
2024-11-06 13:44:55 +0900

    fix: from `config` move to `opt`

--- lua/plugins/meta.lua
+++ lua/plugins/meta.lua
@@ -1,107 +1,121 @@
 return {
+    -- be lazy or something idk
     { "folke/lazy.nvim" },
     {
+        "onsails/lspkind.nvim",
+        opts = {
+            preset = "codicons",
+            symbol_map = {
+                Text = "󰉿",
+                Snippet = "",
+            },
+
+        },
+    },
+    -- babysit some trees idk
+    {
         "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                -- install these on startup
-                ensure_installed = {
-                    "bash",
-                    "comment",
-                    "git_rebase",
-                    "gitcommit",
-                },
-                -- install missing parsers on demand
-                auto_install = true,
+        opts = {
+            -- install these on startup
+            ensure_installed = {
+                "bash",
+                "comment",
+                "git_rebase",
+                "gitcommit",
+            },
+            -- install missing parsers on demand
+            auto_install = true,
 
-                -- semantic code highlighting
-                highlight = {
+            -- semantic code highlighting
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    -- fixes broken highlights inside cmdline window (#3961)
+                    if lang == "vim" then
+                        local bufname = vim.api.nvim_buf_get_name(buf)
+                        return string.match(bufname, "%[Command Line%]")
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+
+            -- builtin indent module have some flaws
+            indent = { enable = false },
+            -- using yati instead while it gets fixed
+            yati = { enable = true },
+
+            textobjects = {
+                select = {
                     enable = true,
-                    disable = function(lang, buf)
-                        -- fixes broken highlights inside cmdline window (#3961)
-                        if lang == "vim" then
-                            local bufname = vim.api.nvim_buf_get_name(buf)
-                            return string.match(bufname, "%[Command Line%]")
-                        end
+                    lookahead = true,
+                    keymaps = {
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["a/"] = "@comment.outer",
+                    },
+                    include_surrounding_whitespace = function(args)
+                        return args.query_string:match("outer")
                     end,
-                    additional_vim_regex_highlighting = false,
                 },
 
-                -- builtin indent module have some flaws
-                indent = { enable = false },
-                -- using yati instead while it gets fixed
-                yati = { enable = true },
-
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["a/"] = "@comment.outer",
-                        },
-                        include_surrounding_whitespace = function(args)
-                            return args.query_string:match("outer")
-                        end,
-                    },
-
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            ["]]"] = "@class.outer",
-                            ["]m"] = "@function.outer",
-                        },
-                        goto_next_end = {
-                            ["]["] = "@class.outer",
-                            ["]M"] = "@function.outer",
-                        },
-                        goto_previous_start = {
-                            ["[["] = "@class.outer",
-                            ["[m"] = "@function.outer",
-                        },
-                        goto_previous_end = {
-                            ["[]"] = "@class.outer",
-                            ["[M"] = "@function.outer",
-                        },
-                    },
-
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["],"] = "@parameter.inner",
-                        },
-                        swap_previous = {
-                            ["[,"] = "@parameter.inner",
-                        },
-                    },
-                },
-
-                textsubjects = {
+                move = {
                     enable = true,
-                    prev_selection = "<BS>",
-                        keymaps = {
-                            ["<CR>"] = "textsubjects-smart",
-                            ["a<CR>"] = "textsubjects-container-outer",
-                            ["i<CR>"] = "textsubjects-container-inner",
-                        },
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]]"] = "@class.outer",
+                        ["]m"] = "@function.outer",
                     },
-
-                endwise = { enable = true },
-
-                context_commentstring = {
-                    enable_autocmd = false,
+                    goto_next_end = {
+                        ["]["] = "@class.outer",
+                        ["]M"] = "@function.outer",
+                    },
+                    goto_previous_start = {
+                        ["[["] = "@class.outer",
+                        ["[m"] = "@function.outer",
+                    },
+                    goto_previous_end = {
+                        ["[]"] = "@class.outer",
+                        ["[M"] = "@function.outer",
+                    },
                 },
 
-                matchup = {
+                swap = {
                     enable = true,
-                    disable_virtual_text = true,
-                    include_match_words = true,
+                    swap_next = {
+                        ["],"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["[,"] = "@parameter.inner",
+                    },
                 },
-            })
+            },
+
+            textsubjects = {
+                enable = true,
+                prev_selection = "<BS>",
+                keymaps = {
+                    ["<CR>"] = "textsubjects-smart",
+                    ["a<CR>"] = "textsubjects-container-outer",
+                    ["i<CR>"] = "textsubjects-container-inner",
+                },
+            },
+
+            endwise = { enable = true },
+
+            context_commentstring = {
+                enable_autocmd = false,
+            },
+
+            matchup = {
+                enable = true,
+                disable_virtual_text = true,
+                include_match_words = true,
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
         end,
     }
 }

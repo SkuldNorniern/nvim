5b6c8ed9b3f0c9cb86bd57d71c9399ec6b1b2bb4
SkuldNorniern <skuldnorniern@gmail.com>
2024-11-06 09:03:31 +0900

    feat: add save/quit keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,13 @@ map({"n", "x", "o"}, "/", "/\\v", { desc = "Regex Search" })
 map({"n", "x", "o"}, "?", "?\\v", { desc = "Reverse Regex Search" })
 map({"n", "x", "o"}, "g/", "/\\v", { desc = "Regex Search" })
 map({"n", "x", "o"}, "g?", "?\\v", { desc = "Reverse Regex Search" })
+
+map("n", "-", "<Cmd>edit %:h<CR>", { desc = "Explorer Parent Dir" })
+map("n", "_", "<Cmd>edit .<CR>", { desc = "Explorer CWD" })
+
+map("n", "<leader>q", "<Cmd>q<CR>", { desc = "Quit" })
+map("n", "<leader>Q", "<Cmd>q!<CR>", { desc = "Force Quit" })
+map("n", "<leader>w", "<Cmd>w<CR>", { desc = "Save" })
+map("n", "<leader>W", "<Cmd>wa<CR>", { desc = "Save All" })
+map("n", "<leader>x", "<Cmd>x<CR>", { desc = "Save & Quit" })
+map("n", "<leader>X", "<Cmd>x!<CR>", { desc = "Force Save & Quit" })
